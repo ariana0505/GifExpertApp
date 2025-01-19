325edad752bf137c10ac747b97b4769312adf4a2
ariana0505 <penaariana075@gmail.com>
2025-01-19 15:36:28 -0500

    Inicio

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AddCategory } from './componentes/AddCategory'
 
 export const GifExpertApp = () => {
-  return (
-    <div>GifExpertApp</div>
+
+  const [categoria, setCategoria] = useState(['One push', 'Dragon Ball',])
+  
+  const onAddCategory = () => {
+    setCategoria(['Valorant',...categoria])
+  }
+
+  return(
+    <>
+    {/* Titulo */}
+    <h1>Gif</h1>
+    {/* Input */}
+    
+    {/* Listado de Gif */}
+    <button onClick={onAddCategory}>Agregar</button>
+    <ol>
+      {
+        categoria.map(category =>{
+          return <li key ={category}>{category}</li>/*tiene que ser unico para qu ayude al Dom para renderizar */
+        })
+      }
+    </ol>
+      {/* gIF item */}
+
+    </>
   )
 }
