da117a0249a869df57086d5d7f726c5ec6dbbc52
ariana0505 <penaariana075@gmail.com>
2025-01-19 15:36:01 -0500

    encabezado

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {GifExpertApp} from './GifExpertApp'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import {GifExpertApp} from './GifExpertApp';
+import './style.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
