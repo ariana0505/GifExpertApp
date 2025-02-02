726f102f5320c51b550da024f0a2307ef05b0060
ariana0505 <penaariana075@gmail.com>
2025-02-01 21:52:31 -0500

    fetch

--- src/componentes/GifGrid.jsx
+++ src/componentes/GifGrid.jsx
@@ -1,16 +1,6 @@
 import React from 'react'
+import { getGif } from '../helpers/GetGifs'
 
-const getGif = async(category) => {
-    const url =`https://api.giphy.com/v1/stickers/search?api_key=896shWlW35idlm0bP8r004oes33ipLrD&q=${category}&limit=20`;
-    const resp =await fetch(url);
-    const {data } = await resp.json();
-    
-    const gifs = data.map(img =>({
-        id: img.id,
-        title: img.title,
-        url: img.images.downsized_medium.url,
-    }))
-}
 
 export const GifGrid = ({category}) => {
     
